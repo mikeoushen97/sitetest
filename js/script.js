404a13c62c7bf6cab5713d93f06da4ae94bc401c
mikeoushen97 <85898683+mikeoushen97@users.noreply.github.com>
2023-01-11 16:53:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const burger = document.querySelectorAll('.burger')
-const mobile = document.querySelectorAll('.adaptive-menu')
+const burger = document.querySelectorAll('.burger');
+const mobile = document.querySelectorAll('.adaptive-menu');
+const menuClick = document.querySelectorAll('.adaptive-menu__link>a');
 
 function menuActive(event) {
     if (event.target.closest('.burger')) {
@@ -10,11 +11,17 @@ function menuActive(event) {
         document.body.classList.toggle('overflow-off')
         setTimeout(() => { mobile[0].style.transform = ('translateX(0px)'); }, 200);
     }
-    // if (!event.target.closest('.burger')) {
-    //     burger[0].classList.remove('active');
-    // }
 }
 
+function removeCls(event) {
+    burger[0].classList.remove('active')
+    mobile[0].classList.remove('menu-active')
+    document.body.classList.remove('overflow-off')
+}
 
+for (let i = 0; i < menuClick.length; i++) {
+    menuClick[i].addEventListener('click', removeCls);
+}
 
 document.addEventListener('click', menuActive);
+
